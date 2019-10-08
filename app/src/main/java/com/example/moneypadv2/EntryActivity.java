1629f5392c1c9ff153805a973057e1cae3212dc0
Teemu <teemu.luhtanen@gmail.com>
2019-10-08 20:57:16 +0300

    Layouttia ja EntryActivity (Backbutton)

--- app/src/main/java/com/example/moneypadv2/EntryActivity.java
+++ app/src/main/java/com/example/moneypadv2/EntryActivity.java
@@ -1,5 +1,6 @@
 package com.example.moneypadv2;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,8 @@ public class EntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Add a new entry");
     }
 }
