b08ec1c9ec07de0e545689a5e0f5eb076676e724
Eetu <penttilaeetu@gmail.com>
2019-10-09 15:20:46 +0300

    ViewList toimii jipii

---
+++ app/src/main/java/com/example/moneypadv2/ViewListActivity.java
@@ -0,0 +1,38 @@
+package com.example.moneypadv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class ViewListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_list);
+
+        final ListView listView = (ListView) findViewById(R.id.listView);
+
+        String[] values = new String[]{ "esimerkki1",
+        "esimerkki2",
+        "esimerkki3"};
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,android.R.id.text1,values);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String itemValue = (String) listView.getItemAtPosition(position);
+                Toast.makeText(ViewListActivity.this, itemValue, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
